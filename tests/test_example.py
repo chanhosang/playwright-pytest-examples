6718f544beb02819bcdda12bd498685e751340af
Chan Ho Sang <hosang.chan@paynet.my>
2021-08-18 15:35:45 +0800

    bug fixed on parsing variables from json file

--- tests/test_example.py
+++ tests/test_example.py
@@ -9,9 +9,32 @@ import pytest
 import json
 
 @pytest.mark.smoke
-def test_goto(page,variables, password):
-    print("name:"+variables.get('name'))
-    print("password:"+password)
-    page.goto(variables.get('url'))
+def test_goto_bing(page,variables, password):
+
+
+    filter_name = 'bing'
+
+    data = variables.get('environments')
+
+
+    if filter_name and data:
+            filter_object = [x for x in data if x['name'] == filter_name]
+            # print(filter_object)
+            if filter_object:
+                json_environment = filter_object[0]
+
+                print("name:"+json_environment['name'])
+                print("url:"+json_environment['url'])
+
+                print("Go to URL: "+json_environment['url'])
+                page.goto(json_environment['url'])
+
+                # Just to try out parsing password from command line
+                print("password:"+password)
+            else:
+                assert False, "The 'name:"+filter_name+"' element not found in the variable file"
+    else:
+        assert False, "The 'environments' element not found in the variable file"
+
 
 
